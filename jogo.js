b736ce1359b798e9767db56618ce23241837ca3e
bruno venancio <jbrunogvenancio@gmail.com>
2020-07-28 20:09:56 -0300

    app pronto

--- jogo.js
+++ jogo.js
@@ -1,6 +1,23 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 10
+var criaMosquitoTempo = 15000
+
+var nivel = window.location.search
+nivel = (nivel.replace('?', ''))
+
+if (nivel === 'normal') {
+	//1500
+	criaMosquitoTempo = 1500
+}else if (nivel === 'dificil') {
+	//1000
+	criaMosquitoTempo = 1000
+}else if (nivel === 'chucknorris') {
+	//750
+	criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -11,13 +28,39 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+	if (tempo < 0) {
+
+		clearInterval(cronometro)
+		clearInterval(Mosquito)
+		window.location.href = 'vitoria.html'
+
+	}else{
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+}, 1000)
+
 
 function posicaoRandomica() {
 
 	//remover o mosquito anterior (caso exista)
+
 	if (document.getElementById('mosquito')) {
 
 	document.getElementById('mosquito').remove()
+
+	//console.log('elemento v' + vidas)
+
+	if (vidas > 3) {
+
+		window.location.href = 'fim_de_jogo.html'
+		
+	}else{
+	document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+	}
+	vidas++
 
 	}
 	var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -36,6 +79,9 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
+	mosquito.onclick = function(){
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
 
