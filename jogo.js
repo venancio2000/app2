b0e9e5fd913b7126167f4a1f9ad3af770037891b
bruno venancio <jbrunogvenancio@gmail.com>
2020-07-27 23:47:32 -0300

    layout faltar pouco

--- jogo.js
+++ jogo.js
@@ -14,6 +14,12 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+	//remover o mosquito anterior (caso exista)
+	if (document.getElementById('mosquito')) {
+
+	document.getElementById('mosquito').remove()
+
+	}
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -25,10 +31,38 @@ function posicaoRandomica() {
 	//criar o elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
 
 	document.body.appendChild(mosquito)
+
+	console.log(ladoAleatorio())
+}
+function tamanhoAleatorio(){
+	var classe = Math.floor(Math.random() * 3)
+
+
+	switch(classe){
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
+	}
+}
+function ladoAleatorio(){
+	var classe = Math.floor(Math.random() * 2)
+	
+
+	switch(classe){
+		case 0:
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
+		
+	}
 }
